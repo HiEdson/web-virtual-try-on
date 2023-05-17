ef9a582ac1a520e6aa9b097c7d65b723a9ae6fbb
Edson <edsoncasimiro264@gmail.com>
2023-05-18 00:10:56 +0300

    added images and the clothing to the product modal and some react events

--- frontend/src/Clothing.js
+++ frontend/src/Clothing.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import './Clothing.css';
+import modelImg from './03615_00.jpg'
+
+
 const Clothing = (props) => {
+    const [targetModel, setTargetModel] = useState(null)
     //props will be the id of that obj
     //we will fetch it from db and then display.
     let Obj = props.obj;
+
 
     let tryOnModal = <div class="modal fade" id="tryOnModal" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
         <div class=" modal-dialog modal-xl">
@@ -17,9 +22,43 @@ const Clothing = (props) => {
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    Show a second modal and hide this one with the button below.
+                    <div className="row">
+                        <div className="col-md-2" style={{ maxHeight: "60vh", overflowY: "scroll" }}>
+                            {/* Should be mapped */}
+                            <button className="mb-2 border-0" onClick={(e)=>{setTargetModel(modelImg)}}>
+                                <img src={modelImg} style={{ width: "100%", height: "200px", objectFit: "fill" }} />
+                            </button>
+                            <button className="mb-2 border-0" onClick={(e)=>{setTargetModel(modelImg)}}>
+                                <img src={modelImg} style={{ width: "100%", height: "200px", objectFit: "fill" }} />
+                            </button>
+                            <button className="mb-2 border-0" onClick={(e) => { setTargetModel("https://images.unsplash.com/photo-1613005798967-632017e477c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80")}}>
+                                <img src="https://images.unsplash.com/photo-1613005798967-632017e477c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" style={{ width: "100%", height: "200px", objectFit: "fill" }} />
+                            </button>
+
+                        </div>
+
+                        <div className="col-md-8">
+                            <div className="row">
+                                <div className="col-md-5">
+                                    {
+                                        !targetModel ?
+                                            <span></span> :
+                                            <img src={targetModel} style={{ width: "100%", height: "350px" }} />
+                                    }
+                                    {/* <img src="https://productimages.hepsiburada.net/s/70/1000/110000011538476.jpg" style={{ width: "100%", height: "350px" }} />  */}
+                                </div>
+                                <div className="col-md-2 d-flex justify-content-center align-items-center">
+                                    <i class="bi bi-plus-circle fs-1 align-middle"></i>
+                                </div>
+                                <div className="col-md-5">
+                                    <img src="https://productimages.hepsiburada.net/s/70/1000/110000011538476.jpg" style={{ width: "100%", height: "350px" }} />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
-                <hr/>
+                <hr />
                 <div class="text-center my-2">
                     <button class="btn btn-primary btn-lg" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Try On <i class="bi bi-magic"></i></button>
                 </div>
@@ -41,7 +80,7 @@ const Clothing = (props) => {
 
                         <div className="col-md-6">
                             <h4>Cerrahpaşa Tıp Fakültesi Model 9 Üç İplik Şardonlu Hoodie</h4>
-                            <hr />
+                            <hr/>
                             <h1>8.936,40 TL <span className="fs-5 text-secondary">/adet</span></h1>
                             <div className="d-flex justify-content-start mt-5">
                                 <span className="text-secondary">Beden:</span>
@@ -87,7 +126,7 @@ const Clothing = (props) => {
                                     <button class="btn btn-outline-secondary" type="button" id="button-addon2">+</button>
                                 </div>
                             </div>
->>>>   ao clicar no dene, open new window and show some models (https://getbootstrap.com/docs/5.3/components/modal/   in extra large modal). from up to dows. Person chooses and we process and show her the results.
+>>>> Connectar com backend, ver como pegar as imagens do back para o front and display it. Start with tests.(tomorow)
 
                             <div className="d-flex justify-content-start">
                                 <span className="text-secondary">Toplam fiyat:</span >
@@ -95,7 +134,7 @@ const Clothing = (props) => {
                             </div>
                             <div className="mt-3 d-flex justify-content-around">
                                 <button data-bs-toggle="modal" data-bs-target="#tryOnModal" className="btn btn-lg m-0" style={{ backgroundColor: "#14243b", color: '#cea949' }}>Bu Kıyafeti dene <i class="bi bi-magic"></i></button>
-                                <button className="btn btn-lg" style={{ backgroundColor: "#14243b", color:'#cea949'}}>Sepete ekle <i class="bi bi-cart"></i></button>
+                                <button className="btn btn-lg" style={{ backgroundColor: "#14243b", color: '#cea949' }}>Sepete ekle <i class="bi bi-cart"></i></button>
                             </div>
                             {tryOnModal}
                         </div>
